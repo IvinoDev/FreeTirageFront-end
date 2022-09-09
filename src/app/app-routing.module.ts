23e735b16eda388d88e4b17bc743e9c75fdcbad1
IvinoDev <kayantao@protonmail.com>
2022-09-09 18:07:18 +0000

    Some work on the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo : "accueil",
+    pathMatch:'full'
+  },
+
+  {path: "accueil", 
+  component: PageAccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
